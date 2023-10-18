8fc3a34e566baee77c45f6d266db9d57acdb7bab
KalemdzievskiV <v.kalemdjievski@gmail.com>
2023-10-18 19:30:18 +0200

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Image from '../assets/img/house-banner.png';
+import Search from '../components/Search';
+
+const Banner = () => {
+  return (
+    <section className='h-full max-h-[640px] mb-8 xl:mb-24'>
+      <div className='flex flex-col lg:flex-row'>
+        <div className='lg:ml-8 xl:ml-[180px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0'>
+          <h1 className='text-4xl lg:text-[58px] font-semibold leading-none mb-6'>
+            <span className='text-violet-700'>MT</span> Center Square
+          </h1>
+          <p className='max-w-[480px] mb-8 text-lg'>
+          Experience unparalleled elegance and convenience in our thoughtfully designed apartments, where every day feels like a getaway.
+          </p>
+        </div>
+        <div className='hidden flex-1 lg:flex justify-end items-end'>
+          <img src={Image} alt='' />
+        </div>
+      </div>
+      <Search />
+    </section>
+  );
+};
+
+export default Banner;
